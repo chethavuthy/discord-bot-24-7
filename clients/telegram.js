a082eb18509df1766a2310cd22abf9198036f9fa
Chetha Vuthy <vuthychetha@gmail.com>
2022-07-12 19:06:18 +0700

    add random quote every day

--- clients/telegram.js
+++ clients/telegram.js
@@ -1,13 +1,25 @@
 const { Telegraf } = require("telegraf");
+const cron = require("node-cron");
+const fetch = require("node-fetch");
 const { telegram } = require("../configs");
-const { getRandomEmoji } = require("../utils");
+const { getRandomEmoji, sendMsgAndLog } = require("../utils");
 
 const bot = new Telegraf(telegram.token);
+const quiteURL = "https://quotes.rest/qod.json?category=inspire";
 
 async function initTelegram() {
   bot.launch();
 
   console.log(`Logged in as telegram bot ${getRandomEmoji()}`);
+
+  cron.schedule("0 0 0 * * *", async () => {
+    const response = await fetch(quiteURL);
+    const quotes = await response.json();
+    const quote = quotes.contents.quotes[0].quote;
+    const author = quotes.contents.quotes[0].author;
+
+    sendMsgAndLog(bot, telegram.groupId, `${quote} - ${author}`);
+  });
 }
 
 module.exports = {
